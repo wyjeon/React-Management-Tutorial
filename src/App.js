845f32c5a00c4e83043f826028e3317d4670ebbe
wyjeon <wyjeon.info@gmail.com>
2020-12-31 20:30:04 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+//웹사이트의 화면에 대한 내용출력을 담당하는 부분
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+      <img src={logo} lat='logo' />
+      <h2>Let's develop management system!</h2>
     </div>
   );
 }
