f7ec7fa27b0d7608a97959ece4bf314335ac5adb
wyjeon <wyjeon.info@gmail.com>
2021-01-01 15:07:59 +0900

    Create Cutomer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 //웹사이트의 화면에 대한 내용출력을 담당하는 부분
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat='logo' />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any/1',
+  'name': '홍길동',
+  'birthday': '000000',
+  'gender': '남자',
+  'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/any/2',
+    'name': '김훈',
+    'birthday': '111111',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/any/3',
+    'name': '이준',
+    'birthday': '222222',
+    'gender': '여자',
+    'job': '교수'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              job={c.job}
+            />
+          );
+        })
+      }
+      </div>
+    )
+  }
 }
 
 export default App;
