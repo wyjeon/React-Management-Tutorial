fddedc9008f86d947c1bc92fcef1987465f53e68
wyjeon <wyjeon.info@gmail.com>
2021-01-01 17:20:37 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -21,34 +21,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/any/1',
-  'name': '홍길동',
-  'birthday': '000000',
-  'gender': '남자',
-  'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/any/2',
-    'name': '김훈',
-    'birthday': '111111',
-    'gender': '남자',
-    'job': '프로그래머'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/any/3',
-    'name': '이준',
-    'birthday': '222222',
-    'gender': '여자',
-    'job': '교수'
-  }
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -65,7 +55,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />); })}
+            {this.state.customers ? this.state.customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />); 
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'})
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/any/1',
+        'name': '홍길동',
+        'birthday': '000000',
+        'gender': '남자',
+        'job': '대학생'
+        },
+        {
+          'id': 2,
+          'image': 'https://placeimg.com/64/64/any/2',
+          'name': '김훈',
+          'birthday': '111111',
+          'gender': '남자',
+          'job': '프로그래머'
+        },
+        {
+          'id': 3,
+          'image': 'https://placeimg.com/64/64/any/3',
+          'name': '이준',
+          'birthday': '222222',
+          'gender': '여자',
+          'job': '교수'
+        }
+      ]);
+})
+
+
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
